733d365d5879f2f6fd418910bddb0260e4254d24
Tim Malseed <t.malseed@gmail.com>
2014-06-26 18:54:00 +1000

    Re=add spotify package name (accidentally removed in previous commit

--- extension/src/main/java/com/simplecity/muzei/music/utils/MusicExtensionUtils.java
+++ extension/src/main/java/com/simplecity/muzei/music/utils/MusicExtensionUtils.java
@@ -47,6 +47,9 @@ public class MusicExtensionUtils {
     // Used in the notification listener service to determine if notification was filled by Spotify
     public static final String SPOTIFY_PACKAGE_NAME = "com.spotify.mobile.android.ui";
 
+    // Used in the notification listener service to determine if notification was filled by Spotify
+    public static final String SPOTIFY_PACKAGE_ALT_NAME = "com.spotify.music";
+
     // Tells the MusicExtensionSource to update itself
     public static final String EXTENSION_UPDATE_INTENT = "com.simplecity.muzei.music.update";
 
