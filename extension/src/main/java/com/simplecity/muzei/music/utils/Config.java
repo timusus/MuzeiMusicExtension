84a952c85da212170776374f6c5c2524db9de108
Tim Malseed <t.malseed@gmail.com>
2015-03-01 14:47:10 +1100

    Update Config.java
    Added a comment to deter people from using this extension's LastFM API key.

--- extension/src/main/java/com/simplecity/muzei/music/utils/Config.java
+++ extension/src/main/java/com/simplecity/muzei/music/utils/Config.java
@@ -2,6 +2,7 @@ package com.simplecity.muzei.music.utils;
 
 public class Config {
 
+    //Please do not use this key, go to Last FM and create you own!!
     public static final String LASTFM_API_KEY = "deb5e4fefed2a96884d938493baec43c";
 
 }
